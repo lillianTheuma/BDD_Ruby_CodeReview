4757449c3511370135c24e1425c52f1801ba5bf0
Student <example@example.com>
2019-12-13 12:16:16 -0800

    Added main script, to call method with arguments from terminal.

---
+++ anagramCheck.rb
@@ -0,0 +1,13 @@
+require './lib/anagram.rb'
+
+anagram = Anagram.new("#{ARGV[0]}")
+response = anagram.anagram("#{ARGV[1]}")
+
+if (response[0])
+  puts("#{ARGV[1]} is an anagram of #{ARGV[0]}")
+elsif (response[1] > 0)
+  puts("#{ARGV[1]} is not an anagram of #{ARGV[0]}")
+  puts("They had #{response[1]} letters in common, though!")
+else
+  puts("#{ARGV[1]} is an antinym of #{ARGV[0]}")
+end
