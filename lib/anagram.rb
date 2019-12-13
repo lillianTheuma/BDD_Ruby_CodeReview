893076029fa9eee53faf6702708b1d2eef36f859
Student <example@example.com>
2019-12-13 11:43:10 -0800

    Improved method functionality. It works! Tests passing 5/5

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -5,15 +5,22 @@ class Anagram
     @text = input
   end
   def anagram(compare)
-    text_a = @text.downcase.split('')
-    compare_a = compare.downcase.split('')
+    text_a = @text.downcase.gsub(/[^a-z]/, '').split('').sort.join('')
+    compare_a = compare.downcase.gsub(/[^a-z]/, '').split('').sort.join('')
 
-    final_a = text_a & compare_a
-    puts(final_a)
-    if (final_a)
-      return [true, text_a.length]
+    text_a.split('').each do |letter|
+      if (compare_a.include?(letter))
+        compare_a.sub!(letter,"")
+        text_a.sub!(letter,"")
+      end
+    end
+
+
+    if ((text_a.length == 0) && (compare_a.length == 0))
+      puts("#{text_a.length}\n")
+      return [true, @text.length]
     else
-      return [false, final_a.length]
+      return [false, (@text.length - text_a.length)]
     end
   end
 end
