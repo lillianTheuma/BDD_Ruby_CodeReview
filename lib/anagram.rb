f0484f3bd9c4dc766909ccb1cad2f5f1264105ae
Student <example@example.com>
2019-12-13 12:16:25 -0800

    Refactoring

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -5,16 +5,17 @@ class Anagram
     @text = input
   end
   def anagram(compare)
+    #removes takes text, downcases, removes non-letters, turns it to an array so I can sort it, then turns it back to a string.
     text_a = @text.downcase.gsub(/[^a-z]/, '').split('').sort.join('')
     compare_a = compare.downcase.gsub(/[^a-z]/, '').split('').sort.join('')
 
+    #goes through letters and each time it finds a match it removes the match from each item, so the remaining values are the letters unique to each string.
     text_a.split('').each do |letter|
       if (compare_a.include?(letter))
         compare_a.sub!(letter,"")
         text_a.sub!(letter,"")
       end
     end
-
 
     if ((text_a.length == 0) && (compare_a.length == 0))
       puts("#{text_a.length}\n")
