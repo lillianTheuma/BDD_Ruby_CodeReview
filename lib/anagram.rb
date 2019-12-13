a56608f51ab0f069154697a139d0130a68c6be76
Student <example@example.com>
2019-12-13 15:29:19 -0800

    redesigned class structure to allow method to take no arguments

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,22 +1,24 @@
 class Anagram
-  attr_accessor(:text)
+  attr_reader(:word1)
+  attr_reader(:word2)
 
-  def initialize(input)
-    @text = input
+  def initialize(word1, word2)
+    @word1 = word1
+    @word2 = word2
   end
-  def anagram(compare)
+  def anagram()
     #removes takes text, downcases, removes non-letters, turns it to an array so I can sort it, then turns it back to a string.
-    text_a = @text.downcase.gsub(/[^a-z]/, '').split('').sort.join('')
-    compare_a = compare.downcase.gsub(/[^a-z]/, '').split('').sort.join('')
+    word1_local = @word1.downcase.gsub(/[^a-z]/, '').split('').sort.join('')
+    word2_local = @word2.downcase.gsub(/[^a-z]/, '').split('').sort.join('')
 
     #goes through letters and each time it finds a match it removes the match from each item, so the remaining values are the letters unique to each string.
-    text_a.split('').each do |letter|
-      if (compare_a.include?(letter))
-        compare_a.sub!(letter,"")
-        text_a.sub!(letter,"")
+    word1_local.split('').each do |letter|
+      if (word2_local.include?(letter))
+        word1_local.sub!(letter,"")
+        word2_local.sub!(letter,"")
       end
     end
     #returns an array. First element resolves to a boolean (whether or not an anagram), second element resolves to an int (number of letters in common)
-    return [((text_a.length == 0) && (compare_a.length == 0)), (@text.length - text_a.length)]
+    return [((word1_local.length == 0) && (word2_local.length == 0)), (@word1.split(' ').join('').length - word1_local.length)]
   end
 end
