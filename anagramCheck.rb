6f14d74a2d8aed7368eaa570e32320de812cf5e6
Student <example@example.com>
2019-12-13 13:50:58 -0800

    Added -man argument option

--- anagramCheck.rb
+++ anagramCheck.rb
@@ -1,13 +1,17 @@
 require './lib/anagram.rb'
 
-anagram = Anagram.new("#{ARGV[0]}")
-response = anagram.anagram("#{ARGV[1]}")
-
-if (response[0])
-  puts("#{ARGV[1]} is an anagram of #{ARGV[0]}")
-elsif (response[1] > 0)
-  puts("#{ARGV[1]} is not an anagram of #{ARGV[0]}")
-  puts("They had #{response[1]} letters in common, though!")
+if ("#{ARGV[0]}" == "-man")
+  puts `cat ./doc/anagram.md`
 else
-  puts("#{ARGV[1]} is an antinym of #{ARGV[0]}")
+  anagram = Anagram.new("#{ARGV[0]}")
+  response = anagram.anagram("#{ARGV[1]}")
+
+  if (response[0])
+    puts("#{ARGV[1]} is an anagram of #{ARGV[0]}")
+  elsif (response[1] > 0)
+    puts("#{ARGV[1]} is not an anagram of #{ARGV[0]}")
+    puts("They had #{response[1]} letters in common, though!")
+  else
+    puts("#{ARGV[1]} is an antinym of #{ARGV[0]}")
+  end
 end
