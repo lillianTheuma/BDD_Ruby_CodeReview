d306f2582cc0ced4b944fe9d1631b2b290f6e596
Student <example@example.com>
2019-12-13 11:03:08 -0800

    updated specs to reflect method change

--- spec/anagram_spec.rb
+++ spec/anagram_spec.rb
@@ -3,18 +3,23 @@ require ('anagram')
 
 describe('String#anagram') do
   it('should return [true, 4] if given two strings containing the same letters.') do
-    expect('test'.anagram('tset')).to(eq([true, 4]))
+    anagram = Anagram.new('test')
+    expect(anagram.anagram('tset')).to(eq([true, 4]))
   end
   it('should return [true, 4] if given two strings with different capitalisations') do
-    expect('TeSt'.anagram('tsET')).to(eq([true,4]))
+    anagram = Anagram.new('TeSt')
+    expect(anagram.anagram('tsET')).to(eq([true,4]))
   end
   it('should return [false, 3] if given two strings that are not anagrams, but have 3 letters in common') do
-    expect('test'.anagram('tast')).to(eq([false,3]))
+    anagram = Anagram.new('tsat')
+    expect(anagram.anagram('tast')).to(eq([false,3]))
   end
   it('should return [false, 0] if given two strings that are not anagrams, and have no letters in common') do
-    expect('test'.anagram('fail')).to(eq([false,0]))
+    anagram = Anagram.new('test')
+    expect(anagram.anagram('fail')).to(eq([false,0]))
   end
   it('should function with words that are not of the same length') do
-    expect('testing'.anagram('check')).to(eq([false, 1]))
+    anagram = Anagram.new('test')
+    expect(anagram.anagram('check')).to(eq([false, 1]))
   end
 end
